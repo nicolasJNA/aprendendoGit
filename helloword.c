7bfa869aa1283b743d9cb9170b0591ee370a04f3
NicolasNJ <nicolasjnascimento1002@gmail.com>
2025-06-10 02:04:36 -0300

    correção de sintax

--- helloword.c
+++ helloword.c
@@ -1,21 +1,22 @@
 //ordenação por inseção
 
-void imprimir(int vetor, int tamanho){
-for(int a =0; a < tam; a++){
+
+#include <stdio.h>
+void imprimir(int vetor[], int tamanho){
+for(int a =0; a < tamanho; a++){
 	printf("%d ", vetor[a]);
 }
 }
-#include <stdio.h>
 int main(){
-int vetor[10] = {5,6,4,8,7,2,1,16,3,4}
+int vetor[10] = {5,6,4,8,7,2,1,16,3,4};
 int copia, indice;
 
 for(int a =1; a<10;a++){
 indice = a;
 copia = vetor[a];
 while(indice > 0 && copia < vetor[indice -1]){
-vetor[indice] = vetor[indice  1];
-indice--
+vetor[indice] = vetor[indice - 1];
+indice--;
 }
 vetor[indice] = copia;
 }
